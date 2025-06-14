1a54f278d1bb02418392f90ba6f92dd10e2271e5
@kathir <kathir200318@gmail.com>
2025-06-14 12:16:14 +0530

    un change path

--- server/server.js
+++ server/server.js
@@ -1,38 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import express from'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+import adminRouter from './routes/AdminRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
+import campaignRouter from './routes/campaignRoutes.js';
+import cartRouter from './routes/cartRoutes.js';
+import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoutes.js';
 import { clerkMiddleware } from '@clerk/express'
-import connectDB from './configs/db.js'
-import userRouter from './routes/userRoute.js'
-import adminRouter from './routes/AdminRoutes.js'
-import connectCloudinary from './configs/cloudinary.js'
-import campaignRouter from './routes/campaignRoutes.js'
-import cartRouter from './routes/cartRoutes.js'
-import addressRouter from './routes/addressRoute.js'
-import orderRouter from './routes/orderRoutes.js'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 4000;
 
-// Vercel: Connect DB in each request (middleware if needed)
-app.use(async (req, res, next) => {
-  if (mongoose.connection.readyState !== 1) {
-    await connectDB()
-  }
-  next()
-})
-
+await connectDB()
 await connectCloudinary()
 
+// Allow multiple orgins
 const allowedOrigins = ['http://localhost:5173']
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({ origin: allowedOrigins, credentials: true }))
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
 app.use(clerkMiddleware())
 
-app.get('/', (req, res) => res.send("API is Working"))
+app.get('/',(req, res) => res.send("API is Working"));
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
 app.use('/api/admin', adminRouter)
@@ -41,6 +38,7 @@ app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
 app.use('/api/order', orderRouter)
 
-// DO NOT USE app.listen
 
-export default app // <---- FOR VERCEL!
+app.listen(port, ()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
