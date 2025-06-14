5949dad42241e7fc1c38d7114be73c83d2eca8b7
@kathir <kathir200318@gmail.com>
2025-06-14 11:57:02 +0530

    connection db change

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,16 +1,16 @@
 import { Inngest } from "inngest";
 import Donor from "../models/Donor.js";
+import connectDB from '../configs/db.js'; // Make sure to import your connection utility
 
-
-// Create a client to send and receive events
 export const inngest = new Inngest({ id: "Life-Line" });
 
-//Inngest Function to save user data to a database
+// Create user
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created' },
-    async ({ event })=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        await connectDB();  // <-- Connect to MongoDB first!
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         const donorData = {
             _id: id,
             email: email_addresses[0].email_address,
@@ -19,38 +19,38 @@ const syncUserCreation = inngest.createFunction(
         }
         await Donor.create(donorData)
     }
-    
-)
+);
 
-// Inngest Function  to delete user from database
+// Delete user
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delet-user-with-clerk'},
-    {event: 'clerk/user.deleted' },
-    async ({ event })=>{
-        const {id} = event.data
-        await Donor.findByIdAndDelete(id)
-    }  
-)
+    { id: 'delete-user-with-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        await connectDB();  // <-- Connect!
+        const { id } = event.data;
+        await Donor.findByIdAndDelete(id);
+    }
+);
 
-// Inngest Function  to update user data from database
+// Update user
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated' },
-    async ({ event })=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
+    { id: 'update-user-from-clerk' },
+    { event: 'clerk/user.updated' },
+    async ({ event }) => {
+        await connectDB();  // <-- Connect!
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
         const donorData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
             image: image_url
         }
-        await Donor.findByIdAndUpdate(id, donorData)
-    }  
-)
+        await Donor.findByIdAndUpdate(id, donorData);
+    }
+);
 
-// Create an empty array where we'll export future Inngest functions
 export const functions = [
-    syncUserCreation, 
+    syncUserCreation,
     syncUserDeletion,
     syncUserUpdation
 ];
