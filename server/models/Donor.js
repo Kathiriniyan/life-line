509e9c18da2880bec071c44ad82c2ab254ddab6b
@kathir <kathir200318@gmail.com>
2025-06-14 12:15:22 +0530

    path changes

--- server/models/Donor.js
+++ server/models/Donor.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const donorSchema = new mongoose.Schema({
     _id: {type: String, required:true},
--- server/server.js
+++ server/server.js
@@ -1,35 +1,38 @@
-import cookieParser from 'cookie-parser';
-import express from'express';
-import cors from 'cors';
-import connectDB from './configs/db.js';
-import 'dotenv/config';
-import userRouter from './routes/userRoute.js';
-import adminRouter from './routes/AdminRoutes.js';
-import connectCloudinary from './configs/cloudinary.js';
-import campaignRouter from './routes/campaignRoutes.js';
-import cartRouter from './routes/cartRoutes.js';
-import addressRouter from './routes/addressRoute.js';
-import orderRouter from './routes/orderRoutes.js';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { clerkMiddleware } from '@clerk/express'
+import connectDB from './configs/db.js'
+import userRouter from './routes/userRoute.js'
+import adminRouter from './routes/AdminRoutes.js'
+import connectCloudinary from './configs/cloudinary.js'
+import campaignRouter from './routes/campaignRoutes.js'
+import cartRouter from './routes/cartRoutes.js'
+import addressRouter from './routes/addressRoute.js'
+import orderRouter from './routes/orderRoutes.js'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 
-const app = express();
-const port = process.env.PORT || 4000;
+const app = express()
 
-await connectDB()
+// Vercel: Connect DB in each request (middleware if needed)
+app.use(async (req, res, next) => {
+  if (mongoose.connection.readyState !== 1) {
+    await connectDB()
+  }
+  next()
+})
+
 await connectCloudinary()
 
-// Allow multiple orgins
 const allowedOrigins = ['http://localhost:5173']
 
-// Middleware configuration
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: allowedOrigins, credentials: true }))
 app.use(clerkMiddleware())
 
-app.get('/',(req, res) => res.send("API is Working"));
+app.get('/', (req, res) => res.send("API is Working"))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/user', userRouter)
 app.use('/api/admin', adminRouter)
@@ -38,7 +41,6 @@ app.use('/api/cart', cartRouter)
 app.use('/api/address', addressRouter)
 app.use('/api/order', orderRouter)
 
+// DO NOT USE app.listen
 
-app.listen(port, ()=>{
-    console.log(`server is running on http://localhost:${port}`)
-})
+export default app // <---- FOR VERCEL!
