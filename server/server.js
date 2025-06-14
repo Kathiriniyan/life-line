03dc54606d607046a80e13c4ae341f09c63a542f
@kathir <kathir200318@gmail.com>
2025-06-14 12:21:21 +0530

    model class remove

--- server/server.js
+++ server/server.js
@@ -3,13 +3,7 @@ import express from'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
-import userRouter from './routes/userRoute.js';
-import adminRouter from './routes/AdminRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
-import campaignRouter from './routes/campaignRoutes.js';
-import cartRouter from './routes/cartRoutes.js';
-import addressRouter from './routes/addressRoute.js';
-import orderRouter from './routes/orderRoutes.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
@@ -31,12 +25,7 @@ app.use(clerkMiddleware())
 
 app.get('/',(req, res) => res.send("API is Working"));
 app.use('/api/inngest', serve({ client: inngest, functions }))
-app.use('/api/user', userRouter)
-app.use('/api/admin', adminRouter)
-app.use('/api/campaign', campaignRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/address', addressRouter)
-app.use('/api/order', orderRouter)
+
 
 
 app.listen(port, ()=>{
